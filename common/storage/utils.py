497193bbc255d3d67861de71a0c310da4d793944
TamoshaytisV <amd.vladas@gmail.com>
2019-08-30 13:00:11 +0300

    get db from models

--- common/storage/utils.py
+++ common/storage/utils.py
@@ -3,17 +3,26 @@ Storage utils.
 """
 
 
-def create_db(db, *args):
+def get_db_from_models(models):
+    dbs = set([model._meta.db for model in models])
+    if len(list(dbs)) == 1:
+        return dbs.pop()
+    raise Exception("Multiple databases are not supported!")
+
+
+def create_db(*args):
     """
     Create db tables from models
     """
+    db = get_db_from_models(args)
     db.create_tables(args)
 
 
-def delete_db(db, *args):
+def delete_db(*args):
     """
     Drops database tables.
     """
+    db = get_db_from_models(args)
     db.drop_tables(args)
 
 
