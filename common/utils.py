67430d965229e989bcf3fa3251794a42e4236271
alexcrownjr <alexcrownjr@gmail.com>
2019-09-03 20:11:56 +0400

    added new time function

--- common/utils.py
+++ common/utils.py
@@ -8,6 +8,7 @@ import math
 def utc_now():
     return datetime.datetime.utcnow().replace(microsecond=0)
 
+
 def is_crossing_actual(crossing_created):
     time_now = utc_now()
     crossing_created = str_to_datetime(crossing_created)
@@ -35,13 +36,21 @@ def limit_step_size_floor(amount, step_qty):
     return math.floor(factor * amount) / factor
 
 
-def floor_current_time(k=4):
-    now = datetime.datetime.now()
-    hours = now.hour
+def floor_time(time, k=4):
+    hours = time.hour
     q, r = divmod(hours, k)
     floor_hours = q * k
+    return str(time.replace(microsecond=0, second=0, minute=0, hour=floor_hours))
 
-    return str(now.replace(microsecond=0, second=0, minute=0, hour=floor_hours))
+
+def floor_datetime(date_time_str, k=4):
+    time = str_to_datetime(date_time_str)
+    return floor_time(time)
+
+
+def floor_current_time(k=4):
+    now = datetime.datetime.now()
+    return floor_time(now)
 
 
 def str_to_datetime(date_time_str):
